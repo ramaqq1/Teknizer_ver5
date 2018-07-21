6e53122581aeb69db73de0a8d6757672fe94db9f
ramaqq <10151032@itk.ac.id>
2018-07-21 09:25:50 +0800

    ...

---
+++ app/src/main/java/com/ramaqq/teknizer/Fragment_changePassActivity.java
@@ -0,0 +1,22 @@
+package com.ramaqq.teknizer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ramaqq on 7/18/2018.
+ */
+
+public class Fragment_changePassActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_changepass, container, false);
+    }
+}
