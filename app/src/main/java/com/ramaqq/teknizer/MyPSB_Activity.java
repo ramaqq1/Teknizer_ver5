06a25fbcd71b7e1e9e580ece7b1e95b57cccc42d
ramaqq <10151032@itk.ac.id>
2018-08-18 13:58:23 +0800

    test

---
+++ app/src/main/java/com/ramaqq/teknizer/MyPSB_Activity.java
@@ -0,0 +1,15 @@
+package com.ramaqq.teknizer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyPSB_Activity extends AppCompatActivity {
+    private static final String TAG = "MyPSB_Activity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypsb);
+    }
+}
---
+++ app/src/main/java/com/ramaqq/teknizer/PointActive_Activity.java
@@ -0,0 +1,15 @@
+package com.ramaqq.teknizer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class PointActive_Activity extends AppCompatActivity {
+    private static final String TAG = "PointActive_Activity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_aktif);
+    }
+}
